6ad030149e42f972bb8c433b2075c54c313c6677
fraserxu <xvfeng123@gmail.com>
2013-08-29 22:27:07 +0800

    rm excerpt from template

--- wp-content/themes/ansbotanics/portfolio-jqueryfilter.php
+++ wp-content/themes/ansbotanics/portfolio-jqueryfilter.php
@@ -4,16 +4,16 @@ Template Name: Portfolio 4 Cols jQuery Filter
 */
 ?>
 <?php get_header();?>
-<script type="text/javascript"> 
+<script type="text/javascript">
 jQuery(document).ready(function($){
-  
+
 	$('ul#filter a').click(function() {
 		$(this).css('outline','none');
 		$('ul#filter .current').removeClass('current');
 		$(this).parent().addClass('current');
- 
+
 		var filterVal = $(this).text().toLowerCase().replace(/ /g,'-');
- 
+
 		if(filterVal == 'all') {
 			$('ul.portfolio-4col-filterable li.hidden').fadeIn('slow').removeClass('hidden');
 		} else {
@@ -25,19 +25,19 @@ jQuery(document).ready(function($){
 				}
 			});
 		}
- 
+
 		return false;
-	}); 
+	});
 });
-</script>   
+</script>
       <?php
         global $post;
-        
+
         $heading_image = get_post_meta($post->ID,"_heading_image",true);
         $bgtext_heading_position = get_post_meta($post->ID,"_bgtext_heading_position",true);
         $page_short_desc = get_post_meta($post->ID,"_page_short_desc",true);
-      ?>      
-      <!-- Page Heading --> 
+      ?>
+      <!-- Page Heading -->
       <div id="page-heading">
         <img src="<?php echo $heading_image ? $heading_image : get_template_directory_uri().'/images/page-heading3.jpg';?>" alt="" />
         <div class="heading-text<?php if ($bgtext_heading_position =="right") echo '-right';?>">
@@ -47,7 +47,7 @@ jQuery(document).ready(function($){
       </div>
       <!-- Page Heading End -->
       <div class="clear"></div>
-      
+
       <div class="center">
         <!-- Main Content Wrapper -->
         <div class="maincontent-full">
@@ -55,9 +55,9 @@ jQuery(document).ready(function($){
           <?php echo stripslashes($portfolio_desc);?>
           <br />
         <!-- Portfolio Filter //-->
-    			<ul id="filter"> 
+    			<ul id="filter">
  			      <li><a class="current" href="#"><span><?php echo __('All','ecobiz');?></span></a></li>
-          	<?php  
+          	<?php
             $categories = get_categories('taxonomy=portfolio_category&orderby=ID&title_li=&hide_empty=0');
             foreach ($categories as $category) {
               echo '<li><a href="#">'.$category->name.'</a></li>';
@@ -65,30 +65,30 @@ jQuery(document).ready(function($){
             ?>
         	</ul>
     		<!-- Portfolio Filter End //-->
-          
+
           <div class="clear"></div>
-          
+
           <ul class="portfolio-4col-filterable">
-          <?php 
+          <?php
           $page = (get_query_var('paged')) ? get_query_var('paged') : 1;
-          $portfolio_items_num  = (get_option('ecobiz_portfolio_items_num')) ? get_option('ecobiz_portfolio_items_num') : 8; 
+          $portfolio_items_num  = (get_option('ecobiz_portfolio_items_num')) ? get_option('ecobiz_portfolio_items_num') : 8;
           $portfolio_order = (get_option('ecobiz_portfolio_order')) ? get_option('ecobiz_portfolio_order') : "date";
-          
+
           query_posts(array( 'post_type' => 'portfolio', 'showposts' => $portfolio_items_num,'paged'=>$page,"orderby" => $portfolio_order,'order'=> 'ASC'));
-          
+
           $counter = 0;
           while ( have_posts() ) : the_post();
           $counter++;
             $pf_link = get_post_meta($post->ID, '_portfolio_link', true );
             $pf_url = get_post_meta($post->ID, '_portfolio_url', true );
             $portfolio_type = get_post_meta($post->ID, '_portfolio_type', true );
-            
+
             echo "<li class=\"";
             $myterms = get_the_terms($post->ID,'portfolio_category');
             foreach ($myterms as $myterm ) {
              echo $myterm->slug."\n";
-            }           
-            echo "\">"; 
+            }
+            echo "\">";
             ?>
               <div class="portfolio-blockimg3">
                 <div class="portfolio-imgbox3">
@@ -99,14 +99,13 @@ jQuery(document).ready(function($){
                 </div>
                 </div>
                 <h4><a href="<?php the_permalink();?>"><?php the_title();?></a></h4>
-                <p><?php echo excerpt(12);?></p>
-                <?php $portfolio_readmore = get_option('ecobiz_portfolio_readmore'); ?>                
-                <a href="<?php the_permalink();?>" class="button"><span><?php echo $portfolio_readmore ? $portfolio_readmore : __('VIEW DETAIL ','ecobiz');?><img src="<?php echo get_template_directory_uri();?>/images/arrow_grey.png" alt="" class="readmore"/></span></a>     
+                <?php $portfolio_readmore = get_option('ecobiz_portfolio_readmore'); ?>
+                <a href="<?php the_permalink();?>" class="button"><span><?php echo $portfolio_readmore ? $portfolio_readmore : __('VIEW DETAIL ','ecobiz');?><img src="<?php echo get_template_directory_uri();?>/images/arrow_grey.png" alt="" class="readmore"/></span></a>
               </div>
-            </li>            
+            </li>
           <?php endwhile;?>
           </ul>
         </div>
         <!-- Main Content Wrapper End -->
-    
+
   <?php get_footer();?>
